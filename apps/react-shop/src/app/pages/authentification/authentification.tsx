2a00088d4308da240d6ecb4524b72989b15d5e95
Eddwiin <maguran.mahendrarajan@gmail.com>
2023-01-30 23:19:28 +0100

    fix router problem

--- apps/react-shop/src/app/pages/authentification/authentification.tsx
+++ apps/react-shop/src/app/pages/authentification/authentification.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import styles from './authentification.module.scss';
 
 /* eslint-disable-next-line */
-export interface AuthentificationProps {}
+export interface AuthentificationProps { }
 
 export function Authentification(props: AuthentificationProps) {
   return (
     <div className={styles['container']}>
       <h1>Welcome to Authentification!</h1>
+      <Outlet />
     </div>
   );
 }
